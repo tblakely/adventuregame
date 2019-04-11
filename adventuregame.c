4bf1d90b8f6b84b7931119432979e6a90ad22c31
John1340 <47426808+John1340@users.noreply.github.com>
2019-04-11 10:16:38 -0500

    Update adventuregame.c

--- adventuregame.c
+++ adventuregame.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+
+void introBeg();
+
 int main()
 {
-  
+  introBeg();
+  printf("Please type what you would like to do.");
+}
+
+void introBeg()
+{
+  printf("\nYou wake to an unfamiliar room with a large metal door in front of you with no handle.");
+  printf("\nThe room is dark, but you can make out a few pieces of furniture and an empty fireplace.");
+}
