d59f244360ed866611129ff985aa18d84068ca86
John1340 <47426808+John1340@users.noreply.github.com>
2019-04-11 10:40:26 -0500

    Update adventuregame.c

--- adventuregame.c
+++ adventuregame.c
@@ -4,12 +4,17 @@ void introBeg();
 
 int main()
 {
+  int inspect;
   introBeg();
-  printf("\n\nPlease type what you would like to do.");
+  printf("\n\nWhat would you like to inspect first?");
+  prinf("\nInput 1 for coffee table, 2 for sofa, 3 for chest.);
+  scanf("%d", &inspect);
+        
 }
 
 void introBeg()
 {
   printf("\nYou wake to an unfamiliar room with a large metal door in front of you with no handle.");
   printf("\nThe room is dark, but you can make out a few pieces of furniture and an empty fireplace.");
+  printf("\nAround the room you see a large wooden chest a leather sofa and a coffee table with a note on it.");
 }
